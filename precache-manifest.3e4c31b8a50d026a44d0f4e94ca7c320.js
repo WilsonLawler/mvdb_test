469776b7d56cb8d87de3abd21abb4176fa914fd1
wilsonLawler957 <liungwave@gmail.com>
2019-01-13 08:46:18 +0800

    deploy testing

---
+++ precache-manifest.3e4c31b8a50d026a44d0f4e94ca7c320.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c852fc4a544a557c907e908977e6f0a0",
+    "url": "/mvdb_test/static/media/logo.c852fc4a.svg"
+  },
+  {
+    "revision": "ef102cbdeb04e19b668e",
+    "url": "/mvdb_test/static/js/runtime~main.ef102cbd.js"
+  },
+  {
+    "revision": "908afda979d4d9d9e0e8",
+    "url": "/mvdb_test/static/js/main.908afda9.chunk.js"
+  },
+  {
+    "revision": "d0c2ffbc54be4650acfa",
+    "url": "/mvdb_test/static/js/1.d0c2ffbc.chunk.js"
+  },
+  {
+    "revision": "908afda979d4d9d9e0e8",
+    "url": "/mvdb_test/static/css/main.a88ba332.chunk.css"
+  },
+  {
+    "revision": "d0c2ffbc54be4650acfa",
+    "url": "/mvdb_test/static/css/1.dd96f194.chunk.css"
+  },
+  {
+    "revision": "b65aba66bc1e060971ee16bc831e5d7b",
+    "url": "/mvdb_test/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/mvdb_test/precache-manifest.3e4c31b8a50d026a44d0f4e94ca7c320.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/mvdb_test/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
